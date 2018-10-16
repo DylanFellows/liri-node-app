0d3c58b575b07020bf11715f7c73dc8e40d1432c
Akinoe <dylan101fellows@yahoo.com>
2018-10-15 19:57:03 -0500

    initial commit

---
+++ liri.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+
+
+ /*You'll use Request to grab data from the [OMDB API](http://www.omdbapi.com) and the [Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)*/
